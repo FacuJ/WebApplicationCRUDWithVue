440aa9891bc1c9cceec95ef2d2d63d7cdc327108
Facundo Jaton <facundoe.jaton@gmail.com>
2020-08-02 22:27:12 -0300

    End of Chapter 6. CRUD in client side finished

--- app.js
+++ app.js
@@ -1,15 +1,41 @@
-Vue.component('mi-primer-componente', {
-    props: ['message'],
-    methods: {
-        sayHi: function(value){
-            alert("Hola " +value);
-        }
-    },
-    template: '<div class="alert alert-primary" role="alert"><button class="btn btn-secondary" v-on:click="sayHi(message)">El botón</button>{{message}}</div>'
-})
 var app = new Vue({
     el: "#app",
     data: {
+        key: '',
+        title: '',
+        description: '',
+        lista: [],
+        searchText:''
+    },
+    computed: {
+        filteredList:function(){
+            var arrayFilter = this.lista;
+            var queryText = this.searchText;
 
+            if(queryText !== ""){
+                arrayFilter = arrayFilter.filter(function(obj){
+                    return (
+                        obj.title.toLowerCase() + '' + obj.description.toLowerCase()
+                    ).indexOf(queryText.toLowerCase()) > -1
+                });
+            }
+            return arrayFilter;
+        }
+    },
+    methods: {
+        create: function (key, title, description) {
+            var item = {
+                key: key,
+                title: title,
+                description: description
+            }
+            app.lista.push(item);
+        },
+        deleteItem: function (key) {
+            var index = app.lista.map(function(obj){
+                return obj.key;
+            }).indexOf(key)
+            app.lista.splice(index,1);
+        }
     }
 })
