1ef8825f9ea333f2290377af27afb389d5cb3a8f
Facundo Jaton <facundoe.jaton@gmail.com>
2020-08-03 20:17:24 -0300

    End of chapter 6

---
+++ firebaseInit.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyBPqAkOtZBIuLNKLIXyB-qDJBFaxrfdxVY",
+    authDomain: "fundamentos-de-vuejs.firebaseapp.com",
+    databaseURL: "https://fundamentos-de-vuejs.firebaseio.com",
+    projectId: "fundamentos-de-vuejs",
+    storageBucket: "fundamentos-de-vuejs.appspot.com",
+    messagingSenderId: "56470905218",
+    appId: "1:56470905218:web:41cf972fbed43b94472f9b"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
